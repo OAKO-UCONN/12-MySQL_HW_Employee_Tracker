19aaaa34def32ad9e129fcbf554d4c6a442d13af
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-18 17:30:58 -0400

    MVC and DB info

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   // Your username
   user: "root",
   // Your password
-  password: "",
+  password: "T)UW%#T_$()qwr-a)jsrtg0-",
   database: "employees"
 });
 
